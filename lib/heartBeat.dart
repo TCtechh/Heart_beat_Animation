7d271aef0ff10ba54f3659b542f69799890cc35a
Tharun Chowdary <tharun744h@gmail.com>
2022-12-03 11:54:37 +0530

    Heart_beat_animation

---
+++ lib/heartBeat.dart
@@ -0,0 +1,95 @@
+import 'package:animator/animator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HeartBeat extends StatefulWidget {
+  const HeartBeat({Key? key}) : super(key: key);
+
+  @override
+  State<HeartBeat> createState() => _HeartBeatState();
+}
+
+class _HeartBeatState extends State<HeartBeat> {
+  @override
+  Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: width/2.8,
+                width: width/2.7,
+                child: Animator<double>(
+                  duration: Duration(
+                      milliseconds: 1000),
+                  cycles: 0,
+                  curve: Curves.elasticIn,
+                  tween: Tween<double>(begin: 20.0,end:25.0),
+                  builder: (context,animatorState,child)=>Icon(
+                    Icons.favorite,size: animatorState.value*5,
+                    color: Color(0xffff5757),
+                  ),
+                ),
+              ),
+              Container(
+                height: width/2.8,
+                width: width/2.7,
+                child: Animator<double>(
+                  duration: Duration(
+                      milliseconds: 1000),
+                  cycles: 0,
+                  curve: Curves.elasticIn,
+                  tween: Tween<double>(begin: 20.0,end:25.0),
+                  builder: (context,animatorState,child)=>Icon(
+                    Icons.favorite,size: animatorState.value*5,
+                    color: Color(0xffff5757),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: width/2.8,
+                width: width/2.7,
+                child: Animator<double>(
+                  duration: Duration(
+                      milliseconds: 1000),
+                  cycles: 0,
+                  curve: Curves.elasticIn,
+                  tween: Tween<double>(begin: 20.0,end:25.0),
+                  builder: (context,animatorState,child)=>Icon(
+                    Icons.favorite,size: animatorState.value*5,
+                    color: Color(0xffff5757),
+                  ),
+                ),
+              ),
+              Container(
+                height: width/2.8,
+                width: width/2.7,
+                child: Animator<double>(
+                  duration: Duration(
+                      milliseconds: 1000),
+                  cycles: 0,
+                  curve: Curves.elasticIn,
+                  tween: Tween<double>(begin: 20.0,end:25.0),
+                  builder: (context,animatorState,child)=>Icon(
+                    Icons.favorite,size: animatorState.value*5,
+                    color: Color(0xffff5757),
+                  ),
+                ),
+              ),
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
